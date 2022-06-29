cbdaf066dbc61ae15d224146b5dc4a02bee4bf35
Frank <frankbsmn@gmail.com>
2022-06-30 00:30:36 +0200

    Added the houses LED strips

--- Receiver_LEDs/Receiver_LEDs.ino
+++ Receiver_LEDs/Receiver_LEDs.ino
@@ -49,8 +49,13 @@ int oldYear = 0;
 // LED simulation -------------
 #include <FastLED.h>
 const int NUM_LEDS = 150;
+const int NUM_LEDS_HOUSES = 150;
+const int NUM_LEDS_CANAL = 40;
+
 const int DATA_PIN = 5;
 CRGB leds[NUM_LEDS];
+CRGB ledsHouse[NUM_LEDS_HOUSES];
+CRGB ledsCanal[NUM_LEDS_CANAL];
 
 struct car {
   float pos;
@@ -69,6 +74,11 @@ const float minSpeed = 0.1;
 float years[] = {2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021};
 float values[] = { 5.478888,  16.689393,  24.800169,  28.267820,  0.000000,  4.516175,  3.221977,  33.419927,  41.540107,  55.402483,  61.239891,  70.529669,  78.164378,  83.490455,  87.174393,  88.004279,  94.107344,  99.372296,  98.675240,  94.991301,  93.707683,  100.000000};
 int yearData = 10;
+
+int amountHouses = 0;
+
+
+
 void transmissionComplete(uint8_t *receiver_mac, uint8_t transmissionStatus) {
   if (transmissionStatus == 0) {
     Serial.println("Data sent successfully");
@@ -107,7 +117,12 @@ void dataReceived(uint8_t *senderMac, uint8_t *data, uint8_t dataLength) {
 
 void setup() {
   Serial.begin(115200);     // initialize serial port
-  FastLED.addLeds<WS2811, 0, GRB>(leds, NUM_LEDS);
+
+  // LED strips
+  FastLED.addLeds<WS2811, 0, GRB>(leds, NUM_LEDS); // port D3
+  FastLED.addLeds<WS2811, 5, GRB>(ledsHouse, NUM_LEDS_HOUSES); // port D1
+  FastLED.addLeds<WS2811, 14, GRB>(ledsHouse, NUM_LEDS_HOUSES); // port D5
+
 
   // clear the leds
   FastLED.clear();  // clear all pixel data
@@ -117,6 +132,16 @@ void setup() {
   for (int i = 0; i <= NUM_LEDS; i++) {
     leds[i] = CRGB::Red;
     delay(int(500.0 / float(NUM_LEDS)));
+    FastLED.show();
+  }
+  for (int i = 0; i <= NUM_LEDS_HOUSES; i++) {
+    ledsHouse[i] = CHSV(25.5, 75 * 255, 57 * 255);
+    delay(int(500.0 / float(NUM_LEDS_HOUSES)));
+    FastLED.show();
+  }
+  for (int i = 0; i <= NUM_LEDS_CANAL; i++) {
+    ledsCanal[i] = CHSV(25.5, 75 * 255, 57 * 255);
+    delay(int(500.0 / float(NUM_LEDS_CANAL)));
     FastLED.show();
   }
 
@@ -168,8 +193,18 @@ void loop() {
     actuateTime = millis();
     FastLED.clear();  // clear all pixel data
 
+    // Show the Canal houses (grachten panten).
+    for (int i = 0; i <= NUM_LEDS_CANAL; i++) {
+      ledsCanal[i] = CHSV(25.5, 75 * 255, 57 * 255);
+      delay(int(500.0 / float(NUM_LEDS_CANAL)));
+      FastLED.show();
+    }
+
     if (selected == whoAmI || selected == ALL) {
       doActuate();
+    }
+    if (selected == HOUSES || selected == ALL) {
+      doActuateHouses();
     }
 
     FastLED.show();
@@ -264,12 +299,12 @@ void doActuate() {
   }
 }
 
-//int indexOfArray(float wantedval, float arr[]) {
-//  Serial.print("length: ");
-//  Serial.println(membersof(arr));
-//  for (int i = 0; i < membersof(arr); i++) {
-//    if (wantedval == arr[i]) {
-//      return i;
-//    }
-//  }
-//}
+// this function is called every actuate interval (200ms) and handles the houses lighting up
+void doActuateHouses() {
+  amountHouses++;
+  if (amountHouses >= NUM_LEDS_HOUSES) amountHouses = 1;
+
+  for (int i = 0; i <= min(amountHouses, NUM_LEDS_HOUSES - 1); i++) {
+    ledsHouse[i] = CHSV(25.5, 75 * 255, 57 * 255);// CRGB::Blue;
+  }
+}
